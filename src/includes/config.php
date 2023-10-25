a36c6b4a0661a6af04b855f6b25712eed41a4e19
rajasekhar336 <85215020+rajasekhar336@users.noreply.github.com>
2023-10-25 17:06:31 +0530

    Update config.php

--- src/includes/config.php
+++ src/includes/config.php
@@ -2,7 +2,7 @@
 //time zone
 date_default_timezone_set('Asia/Kolkata');
 //database connection
-$con=mysqli_connect("172.31.33.119","root","","rajasekhar");
+$con=mysqli_connect("172.31.33.119","root","rajasekhar","covidtmsdb");
 if(mysqli_connect_errno()){
 echo "Connection Fail".mysqli_connect_error();
 }
